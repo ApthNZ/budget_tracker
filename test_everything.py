a3e7ac6792e65eb0e11702a19cf5f45a2fe7f401
ApthNZ <24648796+ApthNZ@users.noreply.github.com>
2020-05-21 20:54:48 +1200

    Add a couple of basic structural tests

---
+++ test_everything.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+
+"""
+Test file for checking necessary directories and files are present.
+"""
+
+import os
+from os.path import expanduser
+
+
+class TestStructure:
+    """Test the structure is in place before we read and write data"""
+
+    def test_dir(self):
+        """Make sure the data directory exists"""
+        home = expanduser("~")
+        assert os.path.exists('{0}/budget_tracker/'.format(home))
+
+    def test_file(self):
+        """Make sure the data csv exists"""
+        home = expanduser("~")
+        assert os.path.exists('{0}/budget_tracker/data.csv'.format(home))
